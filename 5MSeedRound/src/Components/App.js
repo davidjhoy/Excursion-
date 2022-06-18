d243b118a89124512e9f4aa930034127913565dc
david <davidjhoyjr@gmail.com>
2022-06-18 11:03:00 -0400

    Added input field to choose city

--- 5MSeedRound/src/Components/App.js
+++ 5MSeedRound/src/Components/App.js
@@ -3,16 +3,32 @@ import '../App.css';
 import Weather from './Weather';
 
 function App() {
+  const[inputCity, setInputCity] = useState('')
+  const [city, setCity] = useState('Miami')
   
-  const [city, setCity] = useState('London')
-  
+  const handleCityChange = (e)=>{
+    e.preventDefault()
+    setCity(inputCity)
+  }
+  const handleChange = (e)=>{
+    setInputCity(e.target.value)
+  }
 
 
   return (
     <div className="App">
       <header className="App-header">
+        <form onSubmit={handleCityChange}>
+        <input
+        type ="text"
+        id = "cityInput"
+        value ={inputCity}
+        onChange ={handleChange}
+        />
+        <button>Here</button>
+        </form>
       <Weather city ={city}/>
-      
+      {/* <Flights city ={city}/> */}
        
       </header>
     </div>
